eca0d630928f2ac8f423ac20112ff8ad3b143be3
RyogaYamauchi <g1854003@cc.kyoto-su.ac.jp>
2022-12-07 15:37:04 +0900

    レポートの設定

--- MakeConst/MakeConst/MakeConstAnalyzer.cs
+++ MakeConst/MakeConst/MakeConstAnalyzer.cs
@@ -33,7 +33,7 @@ namespace MakeConst
 
         // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
         // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
-        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Title = "循環性複雑度";
         private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
         private const string Category = "Usage";
@@ -112,7 +112,7 @@ namespace MakeConst
             return new SyntaxData()
             {
                 Body = node.ToString(),
-                childSyntaxData = childSyntaxDataList,
+                ChildSyntaxData = childSyntaxDataList,
             };
         }
         
@@ -349,7 +349,7 @@ namespace MakeConst
     class SyntaxData
     {
         public string Body { get; set; }
-        public List<SyntaxData >childSyntaxData { get; set; }
+        public List<SyntaxData >ChildSyntaxData { get; set; }
     }
 
     [Serializable]
